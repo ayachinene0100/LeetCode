bc71a06c64fa7258e17310d9319c4902126820ee
ayachinene0100 <ayachinene0100@163.com>
2022-10-28 12:24:35 +0800

    修改assert参数expected和actul的位置

--- src/test/java/com/ayachinene/leetcode/common/P1Test.java
+++ src/test/java/com/ayachinene/leetcode/common/P1Test.java
@@ -10,9 +10,9 @@ class P1Test {
     void twoSum() {
         P1.Solution s = new P1.Solution();
         assertAll(() -> {
-            assertArrayEquals(s.twoSum(Utils.parseIntArray("2, 7, 11, 15"), 9), new int[] { 0, 1 });
-            assertArrayEquals(s.twoSum(Utils.parseIntArray("3, 2, 4"), 6), new int[] { 1, 2 });
-            assertArrayEquals(s.twoSum(Utils.parseIntArray("3, 3"), 6), new int[] { 0, 1 });
+            assertArrayEquals(new int[] { 0, 1 }, s.twoSum(Utils.parseIntArray("2, 7, 11, 15"), 9));
+            assertArrayEquals(new int[] { 1, 2 }, s.twoSum(Utils.parseIntArray("3, 2, 4"), 6));
+            assertArrayEquals(new int[] { 0, 1 }, s.twoSum(Utils.parseIntArray("3, 3"), 6));
         });
     }
 }
